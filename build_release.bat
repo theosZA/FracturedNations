a99e3cbeb7989a9c133c016f616c6d19fc8f183e
Tim Lawrance <theosZA+repo@googlemail.com>
2014-12-09 21:47:08 +0200

    Fixed loader forgetting mod choice
    Removed spaces from "Fractured Nations" file names which caused loader
    to forget your previous mod choice.

--- build_release.bat
+++ build_release.bat
@@ -6,7 +6,7 @@ echo Building release for Fractured Nations
 mkdir ..\FracturedNations.build\FracturedNations
 xcopy *.* ..\FracturedNations.build\FracturedNations /E /Q
 del ..\FracturedNations.build\FracturedNations\build_release.bat
-move "..\FracturedNations.build\FracturedNations\Fractured Nations.*" ..\FracturedNations.build
+move ..\FracturedNations.build\FracturedNations\FracturedNations.* ..\FracturedNations.build
 "C:\Program Files\7-Zip\7z" a -r FracturedNations.zip ..\FracturedNations.build\*.*
 rmdir ..\FracturedNations.build /s /q
 echo Done
